99cc6e8790b3fe410daa0d3bbb46e6860f284302
Bewizi <toluwalase.bewaji@yahoo.com>
2025-04-04 15:35:37 +0100

    this contains my database config and connecting mysql database with sequelize

---
+++ src/shared/config/database_config.ts
@@ -0,0 +1,32 @@
+import {Sequelize} from "sequelize";
+import config from "./index_config";
+
+const database = new Sequelize(
+    config.database.name,
+    config.database.username,
+    config.database.password,
+    {
+        dialect: "mysql",
+        port: config.database.port,
+        logging: console.log,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+)
+
+const testDatabase = async () => {
+    try {
+        await database.authenticate()
+        console.log("database connection establish")
+    } catch (e) {
+
+    }
+}
+
+testDatabase()
+
+export default database;

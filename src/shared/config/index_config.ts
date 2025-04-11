0ef67a61ae840e3b5c3c7bb86dd27819115952fb
Bewizi <toluwalase.bewaji@yahoo.com>
2025-04-11 22:02:10 +0100

    initial commit

--- src/shared/config/index_config.ts
+++ src/shared/config/index_config.ts
@@ -10,16 +10,16 @@ const getEnv = (name: string) => {
 
 const config: ServerConfig = {
     app: {
-        name: getEnv("APP_NAME"),
-        host: getEnv("APP_HOST"),
-        port: Number(getEnv("APP_PORT"))
+        name: getEnv("APP_NAME") ?? "TASK TODOS",
+        host: getEnv("APP_HOST") ?? "0.0.0.0",
+        port: Number(getEnv("APP_PORT")) ?? "5000",
     },
     database: {
-        name: getEnv("DATABASE_NAME"),
-        port: Number(getEnv("DATABASE_PORT")),
-        username: getEnv("DATABASE_USERNAME"),
-        password: getEnv("DATABASE_PASSWORD"),
-        url: getEnv("DATABASE_URL"),
+        name: getEnv("DATABASE_NAME") ?? "tasks_todo",
+        port: Number(getEnv("DATABASE_PORT")) ?? "3306",
+        username: getEnv("DATABASE_USERNAME") ?? "root",
+        password: getEnv("DATABASE_PASSWORD") ?? "",
+        url: getEnv("DATABASE_URL") ?? "",
     }
 }
 

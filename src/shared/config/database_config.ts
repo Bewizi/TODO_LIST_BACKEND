b0bc13bb0d0ade7100b82f0da140935f0fc5a785
Bewizi <toluwalase.bewaji@yahoo.com>
2025-04-11 21:11:02 +0100

    initial commit

--- src/shared/config/database_config.ts
+++ src/shared/config/database_config.ts
@@ -1,34 +1,29 @@
 import {Sequelize} from "sequelize";
 import config from "./index_config";
 
+// Create a Sequelize instance using the DATABASE_URL
+const database = new Sequelize(config.database.url, {
+    dialect: "mysql",
+    logging: console.log,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+});
 
-const database = new Sequelize(
-    config.database.url,
-    // ,config.database.name,
-    // config.database.username,
-    // config.database.password
-    {
-        dialect: "mysql",
-        // port: config.database.port,
-        logging: console.log,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    }
-)
-
+// Function to test database connection
 const testDatabase = async () => {
     try {
-        await database.authenticate()
-        console.log("database connection establish")
-    } catch (e) {
-
+        await database.authenticate();
+        console.log("✅ Database connection established");
+    } catch (error) {
+        console.error("❌ Unable to connect to the database:", error);
+        process.exit(1); // Optional: Exit the process if DB connection fails
     }
-}
+};
 
-testDatabase()
+testDatabase();
 
 export default database;
